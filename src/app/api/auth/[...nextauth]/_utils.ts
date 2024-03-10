20d6133e5059e3e83d18c9ad35d2716122739008
LukaRusadze <rusadzeluka@gmail.com>
2024-03-10 04:31:06 +0400

    fix: auth adapter ts error

--- src/app/api/auth/[...nextauth]/_utils.ts
+++ src/app/api/auth/[...nextauth]/_utils.ts
@@ -2,12 +2,13 @@ import { AuthOptions } from "next-auth";
 import Google from "next-auth/providers/google";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "~/lib/db";
+import type { Adapter } from "next-auth/adapters";
 
 export const authOptions: AuthOptions = {
   pages: {
     signIn: "/login",
   },
-  adapter: DrizzleAdapter(db),
+  adapter: DrizzleAdapter(db) as Adapter,
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
